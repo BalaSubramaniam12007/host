946bf5a2f5b65ffd6b6645b3edbe26cef5906df0
BALASUBRAMANIAM L <balasubramaniam12007@gmail.com>
2024-11-21 11:20:08 +0530

    Add files via upload

---
+++ blog_code/instagram_basic_display_api/defines.php
@@ -0,0 +1,4 @@
+<?php
+	define( 'INSTAGRAM_APP_ID', '1195271974868846' );
+	define( 'INSTAGRAM_APP_SECRET', '1a7ca5dfcbe52075d1ba30a31b82fea6' );
+	define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://4f74-2403-8600-c090-42-0-4ff-6234-39a3.ngrok-free.app/auth' );
